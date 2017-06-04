a3d68709ba37b038b22eca188a289e37dc076c9b
yasenabdelghany22 <yasenabdelghany2222@gmail.com>
2017-06-04 21:51:17 +0200

    first

---
+++ app/Http/routes.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+Route::post('/task', function(Request $request){
+	$validator = validator::make($request->all(),[
+		'name' => 'required|max:255',
+	]);
+
+	if($validator->fails()) {
+		return redirect('/')
+			->withInput()
+			->withErrors($validator);
+	}
+	
+	$task = new Task;
+	$task->name = $request->name;
+	$task->save();
+	return redirect('/');
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+    //
+});
